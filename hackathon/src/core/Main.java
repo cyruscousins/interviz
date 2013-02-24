db2e61390c3a458081150af3c3ebfa822b470aff
Connor Blanck <connorblanck@gmail.com>
2013-02-23 20:21:33 -0500

    Modularize retrieval of analysis

--- hackathon/src/core/Main.java
+++ hackathon/src/core/Main.java
@@ -15,6 +15,28 @@ import com.sun.xml.internal.ws.api.addressing.WSEndpointReference.Metadata;
 
 public class Main {
 
+	private static TrackAnalysis retrieveAnalysis(File songName){
+		EchoNestAPI enApi = null;
+		try {
+			enApi = new EchoNestAPI("QGBFJRQABCWTIDEG0");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		try {
+            Track track = enApi.uploadTrack(songName);
+            track.waitForAnalysis(30000);
+            if (track.getStatus() == Track.AnalysisStatus.COMPLETE) {
+                return track.getAnalysis();
+            } else {
+            	System.err.println("Trouble analysing track " + track.getStatus());
+            	return null;
+            }
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return null;
+	}
+	
 	public static void main(String[] args){
 		
 		VisualizationFrame frame = new VisualizationFrame(800, 600, null);
@@ -26,36 +48,8 @@ public class Main {
 		final JFileChooser fc = new JFileChooser();
 		fc.showOpenDialog(frame);
 		File song = fc.getSelectedFile();
-
 		if(song != null){
-			EchoNestAPI enApi = null;
-			try {
-				enApi = new EchoNestAPI("QGBFJRQABCWTIDEG0");
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-			try {
-                Track track = enApi.uploadTrack(song);
-                track.waitForAnalysis(30000);
-	            if (track.getStatus() == Track.AnalysisStatus.COMPLETE) {
-	            	System.out.println("Tempo: " + track.getTempo());
-                    System.out.println("Danceability: " + track.getDanceability());
-                    System.out.println("Speechiness: " + track.getSpeechiness());
-                    System.out.println("Liveness: " + track.getLiveness());
-                    System.out.println("Energy: " + track.getEnergy());
-                    System.out.println("Loudness: " + track.getLoudness());
-                    System.out.println();
-                    System.out.println("Beat start times:");
-                    TrackAnalysis analysis = track.getAnalysis();
-                    for (TimedEvent beat : analysis.getBeats()) {
-                        System.out.println("beat " + beat.getStart());
-                    }
-	            } else {
-	            	System.err.println("Trouble analysing track " + track.getStatus());
-	            }
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
+			TrackAnalysis analysis = retrieveAnalysis(song);
 		}
 		for(int i = 0; i < 100000; i++){
 			float time = .01f;
