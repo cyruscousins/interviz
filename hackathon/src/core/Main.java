76e3b2e71a8c4dfd9f94516b01507d440faf4a7b
Connor Blanck <connorblanck@gmail.com>
2013-02-23 20:05:07 -0500

    Working echonest api and output

--- hackathon/src/core/Main.java
+++ hackathon/src/core/Main.java
@@ -6,8 +6,10 @@ import javax.swing.JFileChooser;
 
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.TimedEvent;
 import com.echonest.api.v4.Track;
 import com.echonest.api.v4.Track.AnalysisStatus;
+import com.echonest.api.v4.TrackAnalysis;
 import com.sun.xml.internal.ws.api.addressing.WSEndpointReference.Metadata;
 
 
@@ -25,41 +27,13 @@ public class Main {
 		fc.showOpenDialog(frame);
 		File song = fc.getSelectedFile();
 
-		if(song == null){
-			for(int i = 0; i < 100000; i++){
-				float time = .01f;
-				vis.update(time);
-				try{
-					Thread.sleep((int)(1000 * time));
-				}
-				catch(Exception e){
-					e.printStackTrace();
-				}
-				vis.render();
-			}
-		} else {
-			/*
-			AudioFile songFile = null;
-			
-			try {
-				songFile = AudioFileIO.read(song);
-			} catch (Exception e){
-				e.printStackTrace();
-			}
-			
-			Tag tag = songFile.getTag();
-			System.out.println(tag.getFirst(FieldKey.ARTIST) + 
-			tag.getFirst(FieldKey.ALBUM) + 
-			tag.getFirst(FieldKey.TITLE));
-			*/
+		if(song != null){
 			EchoNestAPI enApi = null;
-			
 			try {
 				enApi = new EchoNestAPI("QGBFJRQABCWTIDEG0");
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
-			
 			try {
                 Track track = enApi.uploadTrack(song);
                 track.waitForAnalysis(30000);
@@ -72,15 +46,27 @@ public class Main {
                     System.out.println("Loudness: " + track.getLoudness());
                     System.out.println();
                     System.out.println("Beat start times:");
+                    TrackAnalysis analysis = track.getAnalysis();
+                    for (TimedEvent beat : analysis.getBeats()) {
+                        System.out.println("beat " + beat.getStart());
+                    }
 	            } else {
 	            	System.err.println("Trouble analysing track " + track.getStatus());
 	            }
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
-			
-			
 		}
-		
+		for(int i = 0; i < 100000; i++){
+			float time = .01f;
+			vis.update(time);
+			try{
+				Thread.sleep((int)(1000 * time));
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			vis.render();
+		}
 	}
 }
