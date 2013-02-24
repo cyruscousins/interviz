489956172bdde5f6b92553f4a854932b23d6f7f4
Cyrus Cousins <cyrus.cousins@tufts.edu>
2013-02-23 22:37:52 -0500

    Updated visualizations: fixed pitch class emission

--- hackathon/src/core/Visualization.java
+++ hackathon/src/core/Visualization.java
@@ -101,7 +101,7 @@ public class Visualization {
 				if(pitches[i] * pitches[i] > rand.nextDouble() + .25f){
 					
 					float x = 32;
-					float y = 50 * (2 + i);
+					float y = 40 * (1 + i);
 					
 					y *= rand.nextInt(2) + 1;
 					
@@ -111,7 +111,7 @@ public class Visualization {
 					float d2x = rand.nextFloat();
 					float d2y = 20; //GRAV
 
-					Polynomial radius = new Polynomial(new float[]{2, .3f, -.225f, .1f, -.075f});
+					Polynomial radius = new Polynomial(new float[]{(float)pitches[i] + 2, .3f, -.225f, .1f, -.075f});
 					
 					float MASS = 3;
 					
